8d2f8bf4d42c7696dfdd6391407a330ccf3d7fee
CR <csernikr@gmail.com>
2020-04-26 19:39:29 +0200

    Init2

---
+++ src/main/java/ol/popup/Popup.java
@@ -0,0 +1,47 @@
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 - 2020 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * 
+ */
+package ol.popup;
+
+import gwt.jswrapper.client.GwtJsWrapper;
+import jsinterop.annotations.JsType;
+import ol.Overlay;
+import ol.OverlayOptions;
+
+/**
+ * @author robi
+ *
+ */
+@JsType(isNative = true, namespace = "ol.Overlay", name = "Popup")
+public class Popup extends Overlay {
+
+	static {
+		GwtJsWrapper.injectJs(PopupClientBundle.INSTANCE.olPopupJs(), true, false);
+		GwtJsWrapper.injectCss(PopupClientBundle.INSTANCE.olPopupCss());
+	}
+
+	public Popup() {
+	}
+
+	public Popup(OverlayOptions overlayOptions) {
+	}
+}
